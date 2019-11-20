0c2256f76bd7e0270d98aa5e6d4898d72b5e1ca3
unknown <saulordf@hotmail.com>
2019-11-20 10:41:04 -0300

    Add_Node errados

--- Branch_and_Bound.py
+++ Branch_and_Bound.py
@@ -3,27 +3,35 @@ from scipy.sparse import csr_matrix
 import Auxiliary_Functions as Af
 import copy
 
+upo_global = []
+
 
 def bandwidth_bnb(matrix=csr_matrix):
     bandwidth = Af.objective_function(matrix)
     k = np.amax(matrix.getnnz(0)-1)
     b = int(np.ceil(k/2))
     while b <= k:
+        print("While : ")
         unselected = list(range(0, matrix.get_shape()[0]))
         adj_list = []
         upo = []
         add_node(matrix, unselected, adj_list, upo, 0, b)
         b += 1
-    return bandwidth
+
+    # print(upo_global)
 
 
 def add_node(matrix, unselected, adj_list, upo, i, b):
+    global upo_global
     matrix_aux = copy.deepcopy(matrix)
+    upo_aux = copy.deepcopy(upo)
     if i == matrix.get_shape()[0]:
-        return matrix_aux
+        print("Entrei no IF")
+        upo_global = upo_aux
+        return upo_aux
 
     for node in unselected:
-        unselected_aux = list(range(0, matrix.get_shape()[0]))
+        unselected_aux = copy.deepcopy(unselected)
         unselected_aux.remove(node)
         adj_list_aux = copy.deepcopy(adj_list)
         for x in matrix_aux[node].indices:
@@ -38,4 +46,9 @@ def add_node(matrix, unselected, adj_list, upo, i, b):
             adj_list_aux.remove(x)
 
         if len(adj_list_aux) <= b:
-            add_node(matrix_aux, unselected_aux, adj_list_aux, i+1, b)
+            print(upo_aux)
+            upo_aux.append(node)
+            upo_general = add_node(matrix_aux, unselected_aux, adj_list_aux, upo_aux, i+1, b)
+            if len(upo_general) > 0:
+                print(upo_general)
+                continue
--- Test_Matrix.py
+++ Test_Matrix.py
@@ -5,12 +5,16 @@ import Auxiliary_Functions
 import Branch_and_Bound as bnb
 
 
-row = np.array([0, 1, 2, 3, 0, 3, 2, 3])
-col = np.array([0, 1, 2, 3, 3, 0, 3, 2])
-data = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+row = np.array([0, 1, 2, 3, 0, 3])
+col = np.array([0, 1, 2, 3, 3, 0])
+data = np.array([1, 1, 1, 1, 1, 1])
 matrix = csr_matrix((data, (row, col)), shape=(4, 4))
-linha = matrix[3]
+print(matrix.toarray())
 print(bnb.bandwidth_bnb(matrix))
+
+#matrix = Auxiliary_Functions.swap_indices(2, 3, matrix)
+#matrix = Auxiliary_Functions.swap_indices(2, 1, matrix)
+#print(matrix.toarray())
 
 # matrix = Auxiliary_Functions.swap_indices(0, 1, matrix)
 # matrix = Auxiliary_Functions.swap_indices(1, 2, matrix)
